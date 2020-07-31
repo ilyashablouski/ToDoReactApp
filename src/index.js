5a1c568c6c09231eb8908338a4577a0422aaa12e
Ilya Shablovskiy <ilya.shablovskiy@gmail.com>
2020-07-31 14:47:19 +0300

    state lesson

--- src/index.js
+++ src/index.js
@@ -1,38 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
+import App from './components/app';
 
-  const items = ['Drink Coffee', 'Build Awesome App'];
-  return (
-    <ul>
-      <li>{items[0]}</li>
-      <li>{items[1]}</li>
-    </ul>
-  );
-}
 
-const AppHeader = () => {
-  return <h1>My Todo List</h1>;
-}
-
-const SearchPanel = () => {
-  return <input placeholder="search"/>
-}
-
-const App = () => {
-
-  const isLoggedIn = true;
-  const loginBox = <span>Log in please</span>;
-
-  return (
-    <div>
-      {loginBox}
-      <AppHeader />
-      <SearchPanel />
-      <TodoList />
-    </div>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
